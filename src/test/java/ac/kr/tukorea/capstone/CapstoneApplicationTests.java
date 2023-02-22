0c7dd476795c3f57c98eea84568511f9160e6001
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-02-22 23:18:54 +0900

    commit

---
+++ src/test/java/ac/kr/tukorea/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.tukorea.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
