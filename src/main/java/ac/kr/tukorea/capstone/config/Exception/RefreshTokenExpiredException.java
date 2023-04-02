0fe61e24d6cc3796e597678cd05515cbd5660794
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-04-02 15:50:22 +0900

    [#11] User 회원가입 및 로그인 API 구현 완료

--- src/main/java/ac/kr/tukorea/capstone/config/Exception/RefreshTokenExpiredException.java
+++ src/main/java/ac/kr/tukorea/capstone/config/Exception/RefreshTokenExpiredException.java
@@ -1,8 +1,7 @@
 package ac.kr.tukorea.capstone.config.Exception;
 
-import lombok.Getter;
 
-@Getter
+
 public class RefreshTokenExpiredException extends RuntimeException{
 
 }
--- src/main/java/ac/kr/tukorea/capstone/config/handler/GlobalExceptionHandler.java
+++ src/main/java/ac/kr/tukorea/capstone/config/handler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package ac.kr.tukorea.capstone.config.handler;
 import ac.kr.tukorea.capstone.config.Exception.InvalidRefreshTokenException;
 import ac.kr.tukorea.capstone.config.Exception.RefreshTokenExpiredException;
 import ac.kr.tukorea.capstone.config.util.MessageForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     /*
     [Exception] Refresh Token이 만료되었을 때
     401 error
  */
     @ExceptionHandler(RefreshTokenExpiredException.class)
     public ResponseEntity<MessageForm> handleRefreshTokenExpiredException(){
+        log.info("Refresh Token이 존재하지 않음");
         MessageForm messageForm = new MessageForm(401, "Refresh token is expired", "failed");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageForm);
     }
@@ -26,6 +31,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(InvalidRefreshTokenException.class)
     public ResponseEntity<MessageForm> handleInvalidRefreshTokenException() {
+        log.info("Refresh Token의 username과 요청한 username이 같지 않음");
         MessageForm messageForm = new MessageForm(401, "Refresh token is invalid", "failed");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageForm);
     }
@@ -33,6 +39,7 @@ public class GlobalExceptionHandler {
     // Runtime error
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<MessageForm> handleRuntimeErrorException(){
+        log.info("기타 Runtime error");
         MessageForm messageForm = new MessageForm(500, "Runtime error", "failed");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageForm);
     }
