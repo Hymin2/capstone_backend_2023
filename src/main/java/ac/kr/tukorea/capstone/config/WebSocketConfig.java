bbc594a3641ccee437e9514da0b048c0d162c615
kiccik <4778009@naver.com>
2023-06-21 20:20:09 +0900

    [#46] 채팅방 웹소켓 -> STOMP 방식으로 변경

--- src/main/java/ac/kr/tukorea/capstone/config/WebSocketConfig.java
+++ src/main/java/ac/kr/tukorea/capstone/config/WebSocketConfig.java
@@ -1,15 +1,29 @@
 package ac.kr.tukorea.capstone.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
-@EnableWebSocket
-@RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
+        //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
+        //origins를 개발 도메인으로 변경하니 잘 동작하였다.
+        //이유는 왜 그런지 아직 찾지 못함
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+        //핸드셰이크 경로
     }
 
+    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
+        // /sub로 시작하는 "destination" 헤더를 가진 메세지를 브로커로 라우팅한다.
+    }
 }
